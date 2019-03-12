5d642e507467cf3f74216dba3a9a08b7bceeb3bd
hpark4 <hpark4@gmail.com>
2019-03-12 11:07:50 -0400

    Update QuotientException.java

--- exception/example1/QuotientException.java
+++ exception/example1/QuotientException.java
@@ -1,37 +1,38 @@
+
 public class QuotientException{
-  public static void main(String [] args)
-  {
+    public static void main(String [] args)
+    {
         java.util.Scanner input = new java.util.Scanner(System.in);
         int num;
         int den;
-        int result  = 0; 
-        String response; 
-    
-    do{
-      
-      try{
-        
-        System.out.print("Enter a nominator integer: ");
-        num = input.nextInt(); 
-        
-        System.out.print("Enter a denominator integer: ");
-        den = input.nextInt();
-        
-        result = num / den;   
-        System.out.println(num + "/ " + den + " = " + result); 
-        
-      }catch(ArithmeticException ae)
-      {
-           System.out.println("You must enter 0 for denominator!!!");  
-      }
-      
-      
-    }while(response.toLowerCase().contains("y"));
-    
-    System.out.println("Good bye"); 
+        int result  = 0;
+        String response = "";
 
-    
+        do{
+
+            try{
+
+                System.out.print("Enter a nominator integer: ");
+                num = input.nextInt();
+
+                System.out.print("Enter a denominator integer: ");
+                den = input.nextInt();
+
+                result = num / den;
+                System.out.println(num + "/ " + den + " = " + result);
+
+            }catch(ArithmeticException ae)
+            {
+                System.out.println("You must enter 0 for denominator!!!");
+            }
+           System.out.println("Do you want to try it again? Type Yes or N0: ");
+            response = input.next(); 
+
+        }while(response.toLowerCase().startsWith("y"));
+
+        System.out.println("Good bye");
+
+
     }
 
 }
-//Hello! This is Dustin!!!
