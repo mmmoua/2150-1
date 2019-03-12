59e8d39f750442b8241ee9c6843203b6932ef99b
hpark4 <hpark4@gmail.com>
2019-03-12 11:02:43 -0400

    Update QuotientException.java

--- exception/example1/QuotientException.java
+++ exception/example1/QuotientException.java
@@ -2,8 +2,34 @@ public class QuotientException{
   public static void main(String [] args)
   {
         java.util.Scanner input = new java.util.Scanner(System.in);
-        System.out.print("Enter a numerator integer: ");
+        int num;
+        int den;
+        int result  = 0; 
+        String response; 
+    
+    do{
+      
+      try{
         
+        System.out.print("Enter a nominator integer: ");
+        num = input.nextInt(); 
+        
+        System.out.print("Enter a denominator integer: ");
+        den = input.nextInt();
+        
+        result = num / den;   
+        System.out.println(num + "/ " + den + " = " + result); 
+        
+      }catch(ArithmeticException ae)
+      {
+           System.out.println("You must enter 0 for denominator!!!");  
+      }
+      
+      
+    }while(response.toLowerCase().contains("y"));
+    
+    System.out.println("Good bye"); 
+
     
     }
 
