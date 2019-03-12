e7d3f80fc82705adbaf82aa580a255762541ccae
hpark4 <hpark4@gmail.com>
2019-03-12 10:17:40 -0400

    Create QuotientException.java

---
+++ exception/example1/QuotientException.java
@@ -0,0 +1,10 @@
+public class QuotientException{
+  public static void main(String [] args)
+  {
+        java.util.Scanner input = new java.util.Scanner(System.in);
+        System.out.print("Enter a numerator integer: ");
+        
+    
+    }
+
+}
